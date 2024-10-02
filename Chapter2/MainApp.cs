5e810dfef883d502c1743c750c93b35c975143b0
prooni <lhw629@naver.com>
2024-10-02 11:09:27 +0900

    Chapter2
    Chapter2

---
+++ Chapter2/MainApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+namespace Chapter2
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("사용법 : Hello.exe <이름>");
+                return;
+            }
+
+
+
+            
+            WriteLine("Hello", args[0]);
+        }
+    }
+}
