a4efc3e4678e566cd500dbcdbb6f3a77579abdd0
prooni <lhw629@naver.com>
2024-10-18 17:36:54 +0900

    Chapter19 ReadAsync, WriteAsync 추가
    Chapter19 ReadAsync, WriteAsync 추가

--- Chapter19/Program.cs
+++ Chapter19/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Synchronize;
 using System;
+using System.Numerics;
 using System.Threading;
 
 namespace BasicThread
@@ -580,11 +581,60 @@ namespace Async
             Console.WriteLine("F");
         }
 
+        //static void Main(string[] args)
+        //{
+        //    Caller();
+
+        //    Console.ReadLine();
+        //}
+    }
+}
+
+namespace AsyncFileIO
+{
+    class MainApp
+    {
+        static async Task<long> CopyAsync(string FromPath, string ToPath)
+        {
+            using (var fromStream = new FileStream(FromPath, FileMode.Open))
+            {
+                long totalCopied = 0;
+
+                using (var toStream = new FileStream(ToPath, FileMode.Create))
+                {
+                    byte[] buffer = new byte[1024];
+                    int nRead = 0;
+                    while((nRead = await fromStream.ReadAsync(buffer, 0, buffer.Length))!=0)
+                    {
+                        await toStream.WriteAsync(buffer, 0, nRead);
+                        totalCopied += nRead;
+                    }
+
+                   
+                }
+
+                return totalCopied;
+            }
+        }
+
+        static async void DoCopy(string FromPath, string ToPath)
+        {
+            long totalCopied = await CopyAsync(FromPath, ToPath);
+            Console.WriteLine($"Copied Total {totalCopied} Bytes");
+        }
+
         static void Main(string[] args)
         {
-            Caller();
+            if(args.Length < 2)
+            {
+                Console.WriteLine("Usage : AsyncFileIO <Source> <Destination>");
+                return;
+            }
+
+            DoCopy(args[0], args[1]);
 
             Console.ReadLine();
         }
     }
+
 }
